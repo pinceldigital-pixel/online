c1dd848eba0dd854f131ed07b1f3b7edd32b904b
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-17 16:58:56 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-// Simple cache for app shell
-const CACHE='radio-mockup-fresh';
+const CACHE='radio-fullcontrols-dial-v1';
 const FILES=['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
