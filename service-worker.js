d7a1d0ee1c1b9d4d5ceb7c12b298a2d76afd6f23
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-17 15:53:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Simple cache for app shell
-const CACHE='radio-mockup-fix-v1';
+const CACHE='radio-mockup-fresh';
 const FILES=['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))); self.clients.claim();});
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(FILES.includes('./'+u.pathname.split('/').pop())){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(FILES.includes('./'+u.pathname.split('/').pop())){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));}});
