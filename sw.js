7035dd290236f75a8575bd704c5dc7e137a2d522
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-18 11:41:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,6 @@ const urlsToCache = [
   '.', // Esto cachea el index.html
   'index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
-  // Los íconos se cachearán cuando se soliciten
 ];
 
 // Evento 'install': se dispara cuando el SW se instala
@@ -25,12 +24,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si está en caché, lo devuelve del caché
         if (response) {
           return response;
         }
-        
-        // Si no, va a la red a buscarlo
         return fetch(event.request);
       }
     )
@@ -38,15 +34,13 @@ self.addEventListener('fetch', event => {
 });
 
 // Evento 'activate': Limpieza de cachés viejos
-// Esto se asegura de que la v1, v2, v3, v4 y v5 se eliminen
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME]; // Solo queremos que exista el caché v6
+  const cacheWhitelist = [CACHE_NAME]; 
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Si el caché no está en nuestra "lista blanca", se borra
             console.log('Borrando caché viejo:', cacheName);
             return caches.delete(cacheName);
           }
